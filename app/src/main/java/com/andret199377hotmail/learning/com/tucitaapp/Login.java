4f635a2cad54d65ca61651121391e0fceb06ec4c
stevendrumm <andret199377@hotmail.com>
2016-06-03 01:13:36 -0500

    Nueva version de tucita

--- app/src/main/java/com/andret199377hotmail/learning/com/tucitaapp/Login.java
+++ app/src/main/java/com/andret199377hotmail/learning/com/tucitaapp/Login.java
@@ -5,57 +5,61 @@ package com.andret199377hotmail.learning.com.tucitaapp;
  */
 public class Login {
 
-        private int id;
+        private int TIPDOCUM;
 
-        private int tipodocumento;
+        private String NUMDOCUM;
 
-        private int numerodocumento;
+        private String APELLIDO1;
 
-        private String nombre1;
+        private String APELLIDO2;
 
-        private String nombre2;
+        private String NOMBRE1;
 
-        private String apellido1;
+        private String NOMBRE2;
 
-        private String apellido2;
+    public Login() {
+
+        this.TIPDOCUM = Integer.parseInt(null);
+        this.NUMDOCUM = null;
+        this.NOMBRE1 = null;
+        this.NOMBRE2 = null;
+        this.APELLIDO1 = null;
+        this.APELLIDO2 = null;
+
+    }
 
 
+    public Login( int tipodocumento, String numerodocumento, String nombre1, String nombre2, String apellido1, String apellido2) {
 
-
-    public Login(int id, int tipodocumento, int numerodocumento, String nombre1, String nombre2, String apellido1, String apellido2) {
-        this.id = id;
-        this.tipodocumento = tipodocumento;
-        this.numerodocumento = numerodocumento;
-        this.nombre1 = nombre1;
-        this.nombre2 = nombre2;
-        this.apellido1 = apellido1;
-        this.apellido2 = apellido2;
+        this.TIPDOCUM = tipodocumento;
+        this.NUMDOCUM = numerodocumento;
+        this.NOMBRE1 = nombre1;
+        this.NOMBRE2 = nombre2;
+        this.APELLIDO1 = apellido1;
+        this.APELLIDO2 = apellido2;
 
     }
 
     // Aquí los métodos get
 
-    public int getId(){
-        return this.id;
 
-    }
     public int gettipo(){
-        return this.tipodocumento;
+        return this.TIPDOCUM;
     }
-    public int getNum(){
-        return this.numerodocumento;
+    public String getNum(){
+        return this.NUMDOCUM;
     }
     public String getNombre1(){
-        return this.nombre1;
+        return this.NOMBRE1;
     }
     public String getNombre2(){
-        return this.nombre2;
+        return this.NOMBRE2;
     }
     public String getApellido1(){
-        return this.apellido1;
+        return this.APELLIDO1;
     }
     public String getApellido2(){
-        return this.apellido1;
+        return this.APELLIDO2;
     }
 
 }
--- app/src/main/java/com/andret199377hotmail/learning/com/tucitaapp/LoginActivity.java
+++ app/src/main/java/com/andret199377hotmail/learning/com/tucitaapp/LoginActivity.java
@@ -34,19 +34,21 @@ import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
 import com.google.gson.JsonSyntaxException;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
 
-import javax.net.ssl.HttpsURLConnection;
+import java.net.URL;
+/*
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+*/
 
 /**
  * A login screen that offers login via email/password.
@@ -119,9 +121,9 @@ public class LoginActivity extends AppCompatActivity {
 
 
         // Store values at the time of the login attempt.
-        Integer tipoDocumento = mTipoDocumentoView.getSelectedItemPosition() + 1;
+        String tipoDocumento = Integer.toString(mTipoDocumentoView.getSelectedItemPosition() + 1);
         String Documento = mDocumentoView.getText().toString();
-        String login = "http://www.tucitaacin.com/autenticacion/recurso/usuarios.php";
+        String login = "http://186.170.16.38/autenticacion/recurso/usuarios.php";
 
         boolean cancel = false;
         View focusView = null;
@@ -148,8 +150,8 @@ public class LoginActivity extends AppCompatActivity {
                 ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
                 NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
                 if (networkInfo != null && networkInfo.isConnected()) {
-                    //Toast.makeText(LoginActivity.this, "tipodocumento="+String.valueOf(tipoDocumento)+"&documento=" + Documento, Toast.LENGTH_LONG).show();
-                    new UserLoginTask(String.valueOf(tipoDocumento), Documento, login).execute();
+                    //Toast.makeText(LoginActivity.this, "tipodocumento="+tipoDocumento+"&documento=" + Documento, Toast.LENGTH_LONG).show();
+                    new UserLoginTask(tipoDocumento, Documento, login).execute();
                 } else {
                     Toast.makeText(this, "Error de conexión", Toast.LENGTH_LONG).show();
                 }
@@ -244,56 +246,49 @@ public class LoginActivity extends AppCompatActivity {
      * Represents an asynchronous login/registration task used to authenticate
      * the user.
      */
-    public class UserLoginTask extends AsyncTask<String, Void, Login> {
+    private class UserLoginTask extends AsyncTask<String, Void, Login> {
+        String tipo;
+        String documento;
+        String login;
 
-        private final String tipodocumento;
-        private final String documento;
-        private final String login;
-
-        UserLoginTask(String tipodocumento, String documento, String login) {
-            this.tipodocumento = tipodocumento;
+        UserLoginTask(String tipoDocumento, String documento, String login){
+            this.tipo = tipoDocumento;
             this.documento = documento;
             this.login = login;
 
         }
 
         @Override
-        protected Login doInBackground(String... params) {
+        protected Login doInBackground(String... parametros) {
             // TODO: attempt authentication against a network service.
 
-            String request = "tipo=" + tipodocumento + "&documento=" + documento;
-            URL login = null;
             HttpURLConnection con = null;
             Login response = null;
-            try {
-                login = new URL(this.login);
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            }
 
             try {
+                String request = "tipo=".concat(this.tipo).concat("&documento=".concat(this.documento));
+                URL login = new URL(this.login);
                 con = (HttpURLConnection) login.openConnection();
                 con.setRequestMethod("POST");
                 con.setDoOutput(true);
                 con.getOutputStream().write(request.getBytes("UTF-8"));
-                con.getInputStream();
-                con.setConnectTimeout(15000);
-                con.setReadTimeout(10000);
+                con.connect();
                 int statusCode = con.getResponseCode();
-                if (statusCode != 200) {
+                if (statusCode == 200) {
                     Gson gson = new Gson();
-                    JsonReader reader = new JsonReader(new InputStreamReader(new BufferedInputStream(con.getInputStream()), "UTF-8"));
-                    JsonParser parser = new JsonParser();
-                    JsonElement jsonResponse = parser.parse(String.valueOf(reader));
+                    InputStream in = new BufferedInputStream(con.getInputStream());
+                    JsonReader reader = new JsonReader(new InputStreamReader(in, "UTF-8"));
+                    reader.beginArray();
 
                     try {
-                        response = gson.fromJson(jsonResponse, Login.class);
-                    } catch (JsonSyntaxException e) { }
-
-
+                        response = gson.fromJson(String.valueOf(reader), Login.class);
+                    } catch (JsonSyntaxException e) {
+                        e.printStackTrace();
+                    }
 
                     reader.endArray();
                     reader.close();
+
 
                 } else {
                     response = null;
@@ -301,8 +296,9 @@ public class LoginActivity extends AppCompatActivity {
                 }
             } catch (IOException e) {
                 e.printStackTrace();
-            } finally {
-                con.disconnect();
+                response = null;
+            }finally{
+                if (con!=null) con.disconnect();
             }
             return response;
         }
