512f8ffaf386effb66a798d46ca98f2bef1269f3
stevendrumm <andret199377@hotmail.com>
2016-06-09 14:54:03 -0500

    Nuevos cambios que incluyen login y logout de la aplicacion.

--- app/src/main/java/com/andret199377hotmail/learning/com/tucitaapp/Principal.java
+++ app/src/main/java/com/andret199377hotmail/learning/com/tucitaapp/Principal.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.support.annotation.DrawableRes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
@@ -30,7 +31,8 @@ import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
-public class Principal extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class Principal extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,
+        HeadlinesFragment.OnHeadlineSelectedListener {
     private SQLiteDatabase db;
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
@@ -61,10 +63,34 @@ public class Principal extends AppCompatActivity implements NavigationView.OnNav
         TabHost tabs=(TabHost)findViewById(android.R.id.tabhost);
         tabs.setup();
 
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create an instance of ExampleFragment
+            HeadlinesFragment firstFragment = new HeadlinesFragment();
+
+            // In case this activity was started with special instructions from an Intent,
+            // pass the Intent's extras to the fragment as arguments
+            firstFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, firstFragment).commit();
+        }
+
+
+
         TabHost.TabSpec spec=tabs.newTabSpec("perfil");
         spec.setContent(R.id.tab1);
         spec.setIndicator("", getApplicationContext().getDrawable(R.drawable.ic_perm_identity_white_24dp));
         tabs.addTab(spec);
+
 
         spec=tabs.newTabSpec("cita");
         spec.setContent(R.id.tab2);
@@ -195,7 +221,6 @@ public class Principal extends AppCompatActivity implements NavigationView.OnNav
         db.update(FeedReaderContract.FeedEntry.TABLE_NAME, valores, selection, selectionArgs);
     }
 
-
     @Override
     public void onStart() {
         super.onStart();
@@ -235,4 +260,39 @@ public class Principal extends AppCompatActivity implements NavigationView.OnNav
         AppIndex.AppIndexApi.end(client, viewAction);
         client.disconnect();
     }
+
+    public void onArticleSelected(int position) {
+        // The user selected the headline of an article from the HeadlinesFragment
+
+        // Capture the article fragment from the activity layout
+        ArticleFragment articleFrag = (ArticleFragment)
+                getSupportFragmentManager().findFragmentById(R.id.article_fragment);
+
+        if (articleFrag != null) {
+            // If article frag is available, we're in two-pane layout...
+
+            // Call a method in the ArticleFragment to update its content
+            articleFrag.updateArticleView(position);
+
+
+        } else {
+            // If the frag is not available, we're in the one-pane layout and must swap frags...
+
+            // Create fragment and give it an argument for the selected article
+            ArticleFragment newFragment = new ArticleFragment();
+            Bundle args = new Bundle();
+            args.putInt(ArticleFragment.ARG_POSITION, position);
+            newFragment.setArguments(args);
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+            // Replace whatever is in the fragment_container view with this fragment,
+            // and add the transaction to the back stack so the user can navigate back
+            transaction.replace(R.id.fragment_container, newFragment);
+            transaction.addToBackStack(null);
+
+            // Commit the transaction
+            transaction.commit();
+        }
+    }
+
 }
---
+++ app/src/main/java/com/andret199377hotmail/learning/com/tucitaapp/cita.java
@@ -0,0 +1,20 @@
+package com.andret199377hotmail.learning.com.tucitaapp;
+
+/**
+ * Created by stevendrumm on 9/06/16.
+ */
+public class cita {
+    static String[] Headlines = new String[]{
+            "7:00 AM",
+            "8:00 AM",
+            "9:00 AM",
+            "10:00 AM"
+    };
+
+    static String[] Articles = new String[]{
+            "Article One\n\nExcepteur pour-over occaecat squid biodiesel umami gastropub, nulla laborum salvia dreamcatcher fanny pack. Ullamco culpa retro ea, trust fund excepteur eiusmod direct trade banksy nisi lo-fi cray messenger bag. Nesciunt esse carles selvage put a bird on it gluten-free, wes anderson ut trust fund twee occupy viral. Laboris small batch scenester pork belly, leggings ut farm-to-table aliquip yr nostrud iphone viral next level. Craft beer dreamcatcher pinterest truffaut ethnic, authentic brunch. Esse single-origin coffee banksy do next level tempor. Velit synth dreamcatcher, magna shoreditch in american apparel messenger bag narwhal PBR ennui farm-to-table.",
+            "Article Two\n\nVinyl williamsburg non velit, master cleanse four loko banh mi. Enim kogi keytar trust fund pop-up portland gentrify. Non ea typewriter dolore deserunt Austin. Ad magna ethical kogi mixtape next level. Aliqua pork belly thundercats, ut pop-up tattooed dreamcatcher kogi accusamus photo booth irony portland. Semiotics brunch ut locavore irure, enim etsy laborum stumptown carles gentrify post-ironic cray. Butcher 3 wolf moon blog synth, vegan carles odd future.",
+            "Article Three\n\nLas Excepsiones en la vida se pagan de muchas maneras",
+            "Article Four\n\nToni Kroos has racked up 50 league wins in a Real Madrid shirt! Do you remember this strike against Rayo last season?"
+    };
+}
